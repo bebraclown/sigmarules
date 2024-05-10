c7f5573d922b2dbaa92bb3b79fcaf7fd5802b3ad
bebraclown <152087529+bebraclown@users.noreply.github.com>
2024-05-10 15:48:31 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+TOKEN = '6902870125:AAGdS0XdWV5ppdP-wpm89BjWAojj0lL7CPs'
+FOLDER_ID = 'b1g16u6gf7gu14qrrne1'
+IAM_TOKEN = 't1.9euelZrGx8zKjZvMjpiTzpCJmZPGku3rnpWaxpTMxsiWkJrMk52akZnGkprl8_cdcQdO-e9AUTZW_N3z910fBU7570BRNlb8zef1656Vmo7OnI-Ox5Kbms-KmpeNlJPN7_zF656Vmo7OnI-Ox5Kbms-KmpeNlJPNveuelZqRiZeTkZjKnpeNi8qLnJqekLXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.jp2vmPkG7zWxgB-ZZy4doFvpsyGNyszwe0CDzYlkS4fdDNeGZE1JIfpS5rIfnjeUSxyNWusjOOsL-VpZOS5ODw'
+ADMINS_IDS = [906863024]
+
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+COUNT_LAST_MSG = 4
+TOKENIZE_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/tokenizeCompletion"
+GPT_MODEL = 'yandexgpt-lite'
+GPT_URL = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+MAX_USER_STT_BLOCKS = 10
+MAX_USER_TTS_SYMBOLS = 5_000
+MAX_USER_GPT_TOKENS = 2_000
+
+LOGS = 'creds/logs.log' 
+DB_FILE = 'messages.db'
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Будь добрым другом пользователя'}]
